adad02b25950fe33eae5f40edc099d0b2ff3550a
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-05-27 16:05:01 -0300

    form template, ModelForm, create form views

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -12,3 +13,11 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        print(request.POST)
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
