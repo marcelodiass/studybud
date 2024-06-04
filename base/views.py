badb26c8916086c80b3167c8c56685a8c06d9bc5
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-04 00:03:38 -0300

    update room method

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -17,7 +17,23 @@ def room(request, pk):
 def create_room(request):
     form = RoomForm()
     if request.method == 'POST':
-        print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
