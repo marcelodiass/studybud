2c0b10878288beea96edcdf00b44f56c2564c8d5
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-06-09 17:39:13 -0300

    feat: registration

--- base/urls.py
+++ base/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+
     path('login/', views.login_page, name="login"),
     path('logout/', views.logout_user, name="logout"),
-    path('', views.home, name="home"),
+    path('register/', views.register_user, name="register"),
+
     path('room/<str:pk>/', views.room, name="room"),
     path('create-room/', views.create_room, name="create-room"),
     path('update-room/<str:pk>', views.update_room, name="update-room"),
